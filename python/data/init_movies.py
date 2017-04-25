e3e649b0dfd7711af62bf377dbef1a38eddca355
nonoroazoro <xiaochao.k@gmail.com>
2017-04-25 14:50:39 +0800

    add features

--- python/data/init_movies.py
+++ python/data/init_movies.py
@@ -1,17 +1,35 @@
-from ..utils import load_json, save_json
+from ..utils import load_json, save_csv, load_csv
 
 
 def prepare():
-    data = load_json("raw_movies")
-    extract_tags(data)
-    return data
+    raw_data = load_json("raw_movies")
+    features = extract_features(raw_data)
+    save_features(features)
+
+    features = load_csv("features")
+    return raw_data
 
 
-def extract_tags(p_raw_data):
+def extract_features(movies):
+    casts = set()
+    countries = set()
     genres = set()
-    people = set()
+    tags = set()
 
-    # for movie in p_raw_data:
-    #     del movie["feature"]
+    for movie in movies:
+        countries.update(movie.get("countries", []))
+        genres.update(movie.get("genres", []))
+        tags.update(movie.get("tags", []))
 
-    # save_json(p_raw_data[0], "movie")
+        for cast in movie.get("casts", []):
+            if "name" in cast:
+                casts.add(cast["name"])
+
+    features = set()
+    features.update(casts, countries, genres, tags)
+    return features
+
+
+def save_features(features):
+    rows = [[feature] for feature in features]
+    save_csv("features", rows=rows)
