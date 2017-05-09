6bea9a8fe9f654d83eaedc6351f6e0a333300a68
nonoroazoro <xiaochao.k@gmail.com>
2017-05-09 15:59:22 +0800

    add clusters

--- python/data/movies_clustering.py
+++ python/data/movies_clustering.py
@@ -11,7 +11,13 @@ def clustering(p_clusters=5):
     X, encodings = __getEncodings()
     labels = KMeans(n_clusters=p_clusters, random_state=8).fit_predict(X)
     score = round(calinski_harabaz_score(X, labels), 2)
-    return (labels, score)
+
+    clusters = [{"movies": []} for _ in range(p_clusters)]
+    for index, label in enumerate(labels):
+        movie = encodings[index]
+        clusters[label]["movies"].append(movie["id"])
+
+    return {"clusters": clusters, "labels": labels, "score": score}
 
 
 @lru_cache()
