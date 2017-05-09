3e54b5041c733ba45c7c93a7572b0925afa29ca6
nonoroazoro <xiaochao.k@gmail.com>
2017-05-09 14:52:20 +0800

    add clustering

--- python/__main__.py
+++ python/__main__.py
@@ -3,5 +3,7 @@ from .data import pre_process, encode_features, clustering
 if __name__ == "__main__":
     # pre_process()
     # encode_features()
-    clustering()
+    results = {}
+    for k in range(5, 31, 5):
+        results[k] = clustering(k)
     print("good")
--- python/data/movies_clustering.py
+++ python/data/movies_clustering.py
@@ -1,10 +1,21 @@
 import numpy as np
-from sklearn.cluster import k_means
+
+from functools import lru_cache
+from sklearn.cluster import KMeans
+from sklearn.metrics import calinski_harabaz_score
 
 from ..utils import load_csv
 
 
-def clustering():
+def clustering(p_clusters=5):
+    X, encodings = __getEncodings()
+    labels = KMeans(n_clusters=p_clusters, random_state=8).fit_predict(X)
+    score = round(calinski_harabaz_score(X, labels), 2)
+    return (labels, score)
+
+
+@lru_cache()
+def __getEncodings():
     encodings = load_csv("encodings")
-    features = np.array([list(item["encoding"]) for item in encodings])
-    pass
+    X = np.array([list(item["encoding"]) for item in encodings], dtype=np.int)
+    return (X, encodings)
