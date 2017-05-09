8b18060306f0dc9cb316d45f3851cca551194dd2
nonoroazoro <xiaochao.k@gmail.com>
2017-05-09 17:01:22 +0800

    add lru_cache

--- python/data/movie_process.py
+++ python/data/movie_process.py
@@ -1,6 +1,7 @@
-from .movie_feature import extract_features, save_features, load_features, encode_features, save_encodings
+from functools import lru_cache
 
 from ..utils import load_json
+from .movie_feature import extract_features, save_features, load_features, encode_features, save_encodings
 
 
 def pre_process():
@@ -11,7 +12,7 @@ def pre_process():
 def __extract_features():
     """extract features of movies to file."""
 
-    movies = load_json("raw_movies")
+    movies = __load_movies()
     features = extract_features(movies)
     save_features(features)
 
@@ -19,7 +20,12 @@ def __extract_features():
 def __encoding_features():
     """encoding features to file."""
 
+    movies = __load_movies()
     features = load_features()
-    movies = load_json("raw_movies")
     encodings = encode_features(features, movies)
     save_encodings(encodings)
+
+
+@lru_cache()
+def __load_movies():
+    return load_json("raw_movies")
