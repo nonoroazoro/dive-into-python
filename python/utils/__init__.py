f666a99e5929a0bebba8a82b7f168658df3c1951
nonoroazoro <xiaochao.k@gmail.com>
2017-05-09 16:20:29 +0800

    refactor

--- python/utils/__init__.py
+++ python/utils/__init__.py
@@ -2,16 +2,26 @@ import csv
 import json
 
 
+def load_json(name):
+    path = "data/in/{}.json".format(name)
+    with open(path, mode="r", encoding="utf-8") as fp:
+        return json.load(fp)
+
+
 def save_json(name, data):
     path = "data/out/{}.json".format(name)
     with open(path, mode="w", encoding="utf-8") as fp:
         json.dump(data, fp, ensure_ascii=False)
 
 
-def load_json(name):
-    path = "data/in/{}.json".format(name)
+def load_csv(name, include_header=True):
+    path = "data/out/{}.csv".format(name)
     with open(path, mode="r", encoding="utf-8") as fp:
-        return json.load(fp)
+        if include_header:
+            fr = csv.DictReader(fp)
+        else:
+            fr = csv.reader(fp)
+        return list(fr)
 
 
 def save_csv(name, rows, headers=None):
@@ -25,13 +35,3 @@ def save_csv(name, rows, headers=None):
             else:
                 fw = csv.writer(fp)
                 fw.writerows(rows)
-
-
-def load_csv(name, include_header=True):
-    path = "data/out/{}.csv".format(name)
-    with open(path, mode="r", encoding="utf-8") as fp:
-        if include_header:
-            fr = csv.DictReader(fp)
-        else:
-            fr = csv.reader(fp)
-        return list(fr)
